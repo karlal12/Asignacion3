b39897ff8c7c0aa3f3ae655a811acab1f9c775a5
MarlonACS <marlon.canche233219@potros.itson.edu.mx>
2025-09-16 19:18:51 -0700

    Insertar Alumno
    Método para insertar alumno.

--- backend/prueba.js
+++ backend/prueba.js
@@ -1,20 +1,39 @@
+const AlumnoDao = require('./dataAccess/alumnoDAO');
+const Alumno = require('./models/alumnoModel');
 const db = require('./config/db');
 
 // Conectar a la base de datos
 db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos.');
+
+  // Ejemplo de inserción de un alumno
+  const nuevoAlumno = new Alumno(null, 'Juan Torres');
+
+  AlumnoDao.insertarAlumno(nuevoAlumno, (err, result) => {
     if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-        return;
+      console.error('Error al insertar el alumno:', err);
+    } else {
+      console.log('Alumno insertado con éxito.');
     }
-    console.log('Conexión exitosa a la base de datos.');
+  });
+
+// Ejemplo de selección de todos los alumnos
+
+// Ejemplo de actualización de un alumno
+
+// Ejemplo de eliminación de un alumno por su ID
 
 
-    // Cierra la conexión cuando hayas terminado
-    db.end((err) => {
+// Cierra la conexión cuando hayas terminado
+db.end((err) => {
         if (err) {
-            console.error('Error al desconectar de la base de datos:', err);
+        console.error('Error al desconectar de la base de datos:', err);
         } else {
-            console.log('Desconexión exitosa de la base de datos.');
+        console.log('Desconexión exitosa de la base de datos.');
         }
     });
 });
